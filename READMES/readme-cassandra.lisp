38c79b7393e8be8181c7c07a4e9951e1396d529a
james anderson <james.anderson@setf.de>
2010-07-21 02:53:43 +0200

    readme edits

--- READMES/readme-cassandra.lisp
+++ READMES/readme-cassandra.lisp
@@ -2,21 +2,24 @@
 
 (in-package :cl-user)
 
-;;; sbcl
-;;; (load "/development/source/library/build-init.lisp")
-;;; (asdf:load-system :thrift)
-;;; (let ((*compile-verbose* nil)) (compile-file "/development/source/library/de/setf/thrift/idl/cassandra-types.lisp"))
-;;; (load "/development/source/library/de/setf/thrift/idl/cassandra-types")
+#+(or ccl sbcl sbcl) /development/source/library/
+(load "build-init.lisp")
 
-;;; remote, for example
+;;; ! first, select the api version in the cassandra system definition
+;;; as only one should be loaded at a time.
+(asdf:load-system :de.setf.cassandra)
+
+(in-package :de.setf.cassandra)
+
 (defparameter *c-location*
   ;; remote
   ;; #u"thrift://ec2-174-129-66-148.compute-1.amazonaws.com:9160"
   ;; local
   #u"thrift://127.0.0.1:9160"
-  )
+  "A cassandra service location - either the local one or a remote service 
+ - always a 'thrift' uri.")
 
-(defparameter *c* (client *c-location*))
+(defparameter *c* (thrift:client *c-location*))
 
 
 (cassandra:describe-keyspaces *c*)
@@ -27,6 +30,8 @@
 
 (cassandra:describe-version *c*)
 ;; => "2.1.0"
+
+(close *c*)
 
 (loop for space in (cassandra:describe-keyspaces *c*)
       collect (loop for key being each hash-key of (cassandra:describe-keyspace *c* space)
@@ -60,3 +65,41 @@
 
 ;;; (describe-cassandra *c-location*)
 
+;;; work with Keyspace2
+
+(defparameter *ks* (keyspace *c-location* :name "Keyspace1"))
+(defparameter *standard2* (make-instance 'column-family :keyspace *ks* :name "Standard2"))
+
+
+;;; simple, column-based access
+
+(insert *standard2* :key "user1" :column "one" :value "2")
+(insert *standard2* :key "user2" :column "one" :value "1")
+
+(get *standard2* :key "user1" :column "one")
+
+
+;;; in terms of attribute-value
+;;; individual
+(attribute-value *standard2* "user1" "one")
+
+(princ (nth-value 1 (ignore-errors (attribute-value *standard2* "user" "one"))))
+
+(dotimes (x 10)
+  (setf (attribute-value *standard2* "user1" (format nil "~:r" x)) (princ-to-string x)))
+
+(loop for x from 0 below 10
+      for column-name = (format nil "~:r" x)
+      collect (cons column-name (attribute-value *standard2* "user1" column-name)))
+
+;;; and 'sliced'
+(attribute-values *standard2* "user1")
+
+(attribute-values *standard2* "user1" :start "ninth" :finish "second")
+
+(attribute-values *standard2* "user1" :start "ninth" :count 2)
+
+(setf (attribute-values *standard2* "user3" :column-names '("some" "little" "details"))
+      '("come" "to" "light"))
+
+(attribute-values *standard2* "user3")
